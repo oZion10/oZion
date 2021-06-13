34502e9620d6839e773871e5dcadb3cdeb73fe65
oZion <61920167+oZion10@users.noreply.github.com>
2021-06-13 02:20:13 +0200

    betting + 2x done

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
 # Blackjack (**WIP**)
 
-The aim of this project is to learn about the rules of blackjack and how rules in card games can be applied in code. This is also my first time doing a GitHub repo😅.
+The aim of this project is to learn about the rules of blackjack and how rules in card games can be applied in code, using only the modules Python has to offer by default. 
+
+This is also my first time doing a GitHub repo 😅.
 
 ---
 ## TODO list
-- ~~getting cards~~
-- ~~hitting~~
-- ~~standing~~
-- betting
-- 2x (double bet, 1 extra card, then stand)
-- optimization
+- ~~Getting cards~~
+- ~~Hitting~~
+- ~~Standing~~
+- ~~Betting~~
+- ~~2x (double bet, 1 extra card, then stand)~~
+- Optimization
--- main.py
+++ main.py
@@ -9,11 +9,12 @@ class Cards():
         self.dealer_cards = []
         self.allow_hit = True
         self.bet = 0
+        self.doubled = False
 
-        random.shuffle(self.deck)
-        self.dealer_cards.append(self.deck.pop())
-        random.shuffle(self.deck)
-        self.dealer_cards.append(self.deck.pop())
+        random.shuffle(self.deck) # Shuffle the deck
+        self.dealer_cards.append(self.deck.pop()) # Give a card to the dealer
+        random.shuffle(self.deck) # Shuffle the deck again
+        self.dealer_cards.append(self.deck.pop()) # Give a card to the dealer again
     
     def total(self, deck):
         self.card_sum = 0
@@ -32,48 +33,45 @@ class Cards():
     def generate(self):
         global running
         while True:
-            try: self.bet = int(input(f"{colorObj.PURPLE}How much would you like to bet? $"))
+            try: self.bet = int(input(f"{colorObj.PURPLE}How much would you like to bet? {colorObj.RESET}$"))
             except ValueError: print(f"{colorObj.YELLOW}Incorrect value, try again{colorObj.RESET}")
             else: break
 
-        random.shuffle(self.deck)
-        self.cards.append(self.deck.pop())
-        random.shuffle(self.deck)
-        self.cards.append(self.deck.pop())
-        print(f"{colorObj.RESET}Starting card sum: {colorObj.BOLD}{self.total(self.cards)}{colorObj.RESET}\n{self.cards}\n")
+        random.shuffle(self.deck) # Shuffle the deck
+        self.cards.append(self.deck.pop()) # Give a card to the player
+        random.shuffle(self.deck) # Shuffle deck
+        self.cards.append(self.deck.pop()) # Give a card to the player again
+        print(f"Your cards:", self.total(self.cards), self.cards, "\n")
         if self.total(self.cards) == 21:
             print(f"{colorObj.GREEN}Blackjack! You win!{colorObj.RESET}")
             running = False
 
 
     def hit(self):
-            random.shuffle(self.deck)
-            new_card = self.deck.pop()
+            random.shuffle(self.deck) # Shuffle the deck
+            new_card = self.deck.pop() # Get a new card for the player
             print(f"You got: {new_card}")
             self.cards.append(new_card)
-            print(self.total(self.cards), self.cards, "\n")
+            print("Your cards:", self.total(self.cards), self.cards, "\n")
             del new_card
 
     def score(self):
+        global running
         if self.allow_hit:
             print(f"Dealer cards:", self.total([self.dealer_cards[0], 10]), [self.dealer_cards[0], "?"])
         else:
             print(f"Dealer cards:", self.total(self.dealer_cards), self.dealer_cards)
         print(f"Your cards:", self.total(self.cards), self.cards, "\n")
         
-    def blackjack(self):
-        global running
         if self.total(self.cards) == 21:
             print(f"{colorObj.GREEN}Blackjack! You win!{colorObj.RESET}")
             running = False
 
-        elif self.total(self.dealer_cards) == 21:
+        elif self.total(self.dealer_cards) == 21 and not self.allow_hit:
             print(f"{colorObj.RED}Dealer got a Blackjack, you lose!{colorObj.RESET}")
             running = False
 
-    def general(self):
-        global running
-        if self.total(self.cards) > 21:
+        elif self.total(self.cards) > 21:
             print(f"{colorObj.RED}You busted! You lose!{colorObj.RESET}")
             running = False
 
@@ -109,12 +107,22 @@ class Cards():
             print(f"{colorObj.RED}Your score is lower than the dealers. You lose!{colorObj.RESET}")
             running = False
         
-        elif self.total(self.cards) > self.total(self.dealer_cards): # If player sum is higher than the dealers
+        elif self.total(self.cards) > self.total(self.dealer_cards) and self.total(self.cards) < 21: # If player sum is higher than the dealers
             dealer_sum_check()
             print(f"{colorObj.GREEN}Your score is higher than the dealers. You win!{colorObj.RESET}")
             running = False
 
-        print(self.total(self.cards), self.cards, "\n")
+        print("Your cards:", self.total(self.cards), self.cards, "\n")
+    
+    def double(self): #FIXME
+        self.bet *= 2
+        print(f"{colorObj.CYAN}You have doubled your bet to {colorObj.BOLD}{self.bet}${colorObj.RESET}{colorObj.CYAN}!{colorObj.RESET}")
+        time.sleep(1)
+        self.hit()
+        playerObj.score()
+        if running:
+            time.sleep(1)
+            self.stand()
 
 class Color():
     def __init__(self):
@@ -134,11 +142,13 @@ colorObj = Color()
 playerObj = Cards()
 
 playerObj.generate()
-print("Dealer cards:", playerObj.total(playerObj.dealer_cards), [playerObj.dealer_cards[0], "?"], "\n")
+print("Dealer cards:", playerObj.total([playerObj.dealer_cards[0], 10]), [playerObj.dealer_cards[0], "?"], "\n")
 
 while running:
-    choice = input(f"{colorObj.BLUE}[{colorObj.BOLD}H{colorObj.RESET}{colorObj.BLUE}]it or [{colorObj.BOLD}S{colorObj.RESET}{colorObj.BLUE}]tand? {colorObj.RESET}").lower()
-    if choice in ["h", "hit", "s", "stand"]:
+    choice = input(f"""{colorObj.CYAN}[{colorObj.BOLD}H{colorObj.RESET}{colorObj.CYAN}]it, 
+[{colorObj.BOLD}S{colorObj.RESET}{colorObj.CYAN}]tand {colorObj.RESET}{colorObj.CYAN}or
+[{colorObj.BOLD}D{colorObj.RESET}{colorObj.CYAN}]ouble? {colorObj.RESET}""").lower()
+    if choice in ["h", "hit", "s", "stand", "d", "double"]:
         if choice in ["h", "hit"]:
             if playerObj.allow_hit:
                 playerObj.hit()
@@ -148,9 +158,12 @@ while running:
         elif choice in ["s", "stand"]:
             playerObj.allow_hit = False
             playerObj.stand()
+        
+        elif choice in ["d", "double"]:
+            playerObj.doubled = True
+            playerObj.double()
+            playerObj.allow_hit = False
 
-        playerObj.score()
-        playerObj.blackjack()
-        playerObj.general()
+        if not playerObj.doubled: playerObj.score()
     else:
         print(f"{colorObj.RED}Invalid choice{colorObj.RESET}")
